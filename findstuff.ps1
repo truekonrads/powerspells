60daf3565107b699a5601a0d1a6c562a23a2c1df
Konrads Smelkovs <konrads.smelkovs@kpmg.co.uk>
2018-02-21 18:25:03 +0000

    Improvements - support for max file size etc

--- findstuff.ps1
+++ findstuff.ps1
@@ -1,10 +1,10 @@
 
-function Compress{
+function Compress {
     [CmdletBinding()]
     param(
-    [Parameter(Mandatory=$True, ValueFromPipeline=$True)]
-    [ValidateNotNullOrEmpty()]
-    [byte[]]$bytes
+        [Parameter(Mandatory = $True, ValueFromPipeline = $True)]
+        [ValidateNotNullOrEmpty()]
+        [byte[]]$bytes
     )
 
     [System.IO.MemoryStream] $output = New-Object System.IO.MemoryStream;
@@ -15,18 +15,18 @@ function Compress{
     
 }
 
-function ToBytes{
+function ToBytes {
     [CmdletBinding()]
     param(
-    [Parameter(Mandatory=$True, ValueFromPipeline=$True)]
-    [ValidateNotNullOrEmpty()]
-    [string] $b
+        [Parameter(Mandatory = $True, ValueFromPipeline = $True)]
+        [ValidateNotNullOrEmpty()]
+        [string] $b
     )
     [System.Text.Encoding]::UTF8.GetBytes($b)
 }
 
-function Find-InterestingFiles{
-<#
+function Find-InterestingFiles {
+    <#
     .SYNOPSIS
         Find interesting files in some folders and return those
     .PARAMETER location
@@ -47,55 +47,70 @@ function Find-InterestingFiles{
 #>
     [CmdletBinding()]
     param(
-        [Parameter(Position=0,mandatory=$true)]
+        [Parameter(Position = 0, mandatory = $true)]
         [string] $location,
-        [Parameter(Position=1,mandatory=$false)]
-        [string[]] $exts=("*.bat", "*.vbs", "*.cmd", "*.ps1", "*.sh", "*.pl", "*.txt","*.xml"),
-        [Parameter(Position=2,mandatory=$false)]
-        [string[]] $interesting=("user","pass","net use","login"),
-        [Parameter(Position=3,mandatory=$false)]
-        [Bool] $packfiles=$false,
-        [Parameter(Position=4,mandatory=$false)]
-        [Bool] $compress=$false,
-        [Parameter(Position=5,mandatory=$false)]
-        [string] $outputfile="",
-        [Parameter(mandatory=$false)]
-        [string] $LocationIsServer=$false
+        [Parameter(Position = 1, mandatory = $false)]
+        [string[]] $exts = ("*.bat", "*.vbs", "*.cmd", "*.ps1", "*.sh", "*.pl", "*.txt", "*.xml"),
+        [Parameter(Position = 2, mandatory = $false)]
+        [string[]] $interesting = ("user", "pass", "net use", "login"),
+        [Parameter(Position = 3, mandatory = $false)]
+        [Bool] $packfiles = $false,
+        [Parameter(Position = 4, mandatory = $false)]
+        [Bool] $compress = $false,
+        [Parameter(Position = 5, mandatory = $false)]
+        [string] $outputfile = "",
+        [Parameter(mandatory = $false)]
+        [bool] $LocationIsServer = $false,
+        [Parameter(mandatory = $false)]
+        [string] $maxfilesize = "2mb"
     )
-    $outputbuffer=""
-    if ($LocationIsServer){
-        $shares=(net view $location /ALL) | % { if($_.IndexOf(' Disk ') -gt 0){ $_.Split('  ')[0] } }
-        $fullLocations=$shares | % {"$($location)\$_"}
-    }else{
-        $fullLocations=@($location)
+    $outputbuffer = ""
+    
+    if ($LocationIsServer) {
+
+        $shares = (net view $location /ALL) | % { if ($_.IndexOf(' Disk ') -gt 0) { $_.Split('  ')[0] } }
+        $fullLocations = $shares | % {"$($location)\$_"}
+    }
+    else {
+        $fullLocations = @($location)
     }
     # $fullLocations
-    $fullLocations | Get-ChildItem  -Recurse -Include $exts -File -ErrorAction "SilentlyContinue"| % {
-        $contents = [io.file]::ReadAllText($_.FullName);
-        $fullname = $_.FullName;
-                
-        if ($contents -match ($interesting -join "|")){
-            if ($packfiles){
-                $outputbuffer+="---BEGIN $($fullname) ---`n$($contents)`n---END $($fullname)---`n"
-            }else{
-                $o=New-Object PSObject -Property @{FullName = $fullname; contents=$contents}                
-                $o
-            }        
+    $fullLocations | % { 
+        Write-Host "Processing share $_"
+        Get-ChildItem $_ -Recurse -Include $exts -File -ErrorAction "SilentlyContinue"| % {
+            if ( (Get-Item $_.FullName).Length -lt $maxfilesize){
+                $contents = [io.file]::ReadAllText($_.FullName);
+                $fullname = $_.FullName;
+                Write-Host "Processing $fullname"
+                if ($contents -match ($interesting -join "|")) {
+                    if ($packfiles) {
+                        $outputbuffer += "---BEGIN $($fullname) ---`n$($contents)`n---END $($fullname)---`n"
+                    }
+                    else {
+                        $o = New-Object PSObject -Property @{FullName = $fullname; contents = $contents}                
+                        $o
+                    }        
+                }
         }
-        
-    } # end Get-ChildItem
+
+        } # end of Get-ChildItem loop
+    } # end of share loop
     if ($packfiles) {
-        if ($compress){
-            $compressed = Compress ($outputbuffer |ToBytes)
-            if ($outputfile){
+        if ($compress) {
+            $compressed = Compress (ToBytes $outputbuffer)
+            if ($outputfile) {
                 $compressed | Set-Content $outputfile -Encoding Byte
-            }else{
+            }
+            else {
                 $compressed #do with byte array what you please
             }
-        }else{ # don't compresss
-            if ($outputfile){
+        }
+        else {
+            # don't compresss
+            if ($outputfile) {
                 $outputbuffer | Set-Content $outputfile -Encoding Byte
-            }else{
+            }
+            else {
                 $outputbuffer #do with the buffer what you want
             }
             
